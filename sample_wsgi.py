cb6678f8f2895dcf5885338e53aac364668cd6cb
dkob619 <dkob619@gmail.com>
2018-05-23 12:34:19 +0100

    Modiefied sample_wsgi.py file

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -2,10 +2,15 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
-
 application = StaticFilesHandler(get_wsgi_application())
 
 
 
 
-path =
+path = 'home/pythonanywhereusername/projectname' #use your python anywhere username here... 
+if path not in sys.path:
+	sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'	
+
+
